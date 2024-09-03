fcad97c379aa18174ac7aa297e9993bd6f7ea0ad
tawheed78 <tawheed.chilwan55@gmail.com>
2024-09-03 22:39:34 +0530

    Added category field in info model

--- info/models.py
+++ info/models.py
@@ -2,6 +2,29 @@ from django.db import models
 
 # Create your models here.
 class Info(models.Model):
+    CATEGORY_CHOICES = [
+        ('free_mint', 'Free Mint'),
+        ('pfp', 'PFP'),
+        ('collectible', 'Collectible'),
+        ('charity', 'Charity'),
+        ('3d', '3D'),
+        ('art', 'Art'),
+        ('pixel', 'Pixel'),
+        ('metaverse', 'Metaverse'),
+        ('membership', 'Membership'),
+        ('music', 'Music'),
+        ('gaming', 'Gaming'),
+        ('alpha', 'Alpha'),
+        ('utility', 'Utility'),
+        ('memecoin', 'Memecoin'),
+        ('defi', 'Defi'),
+        ('dex', 'Dex'),
+        ('dao', 'Dao'),
+        ('generative', 'Generative'),
+        ('giveaway', 'Giveaway'),
+        ('sports', 'Sports'),
+        ('other', 'Other'),
+    ]
     info_id = models.AutoField(primary_key=True, unique=True)
     title = models.CharField(max_length=100, null=True, blank=True)
     desc = models.CharField(max_length=100, null=True, blank=True)
@@ -12,7 +35,7 @@ class Info(models.Model):
     # background_image
     background_image= models.ImageField(upload_to ='uploads/% Y/% m/% d/',null=True, blank=True)
     blockchain = models.CharField(max_length=100, null=True, blank=True)
-    category = models.CharField(max_length=100, null=True, blank=True)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, null=False, default='other')
     supply = models.IntegerField(null=True, blank=True)
     private_mint_price = models.IntegerField(null=True, blank=True)
     public_mint_price = models.IntegerField(null=True, blank=True)
